f799507d57e424c61c2b483fd2777bd90dda9094
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2025-12-20 10:43:42 +0800

    first commit

---
+++ app/public/wp-content/themes/wp-devs/index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="search-box">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Main Menu
+                </nav>
+            </section>
+        </header>
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <section class="home-blog">
+                        home-blog
+                    </section>
+                </main>
+            </div>
+        </div>
+        <footer class="site-footer">
+            Footer
+        </footer>
+    </div>
+</body>
+</html>
