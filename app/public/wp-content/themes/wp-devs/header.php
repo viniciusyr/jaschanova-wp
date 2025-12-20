e6f481e9f2f26e510edb4d06493f8a6ef77d97b7
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2025-12-20 11:17:25 +0800

    feature: header

---
+++ app/public/wp-content/themes/wp-devs/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="search-box">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Main Menu
+                </nav>
+            </section>
+        </header>
--- app/public/wp-content/themes/wp-devs/index.php
+++ app/public/wp-content/themes/wp-devs/index.php
@@ -1,27 +1,5 @@
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset')?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body <?php body_class(); ?>>
-    <div id="page" class="site">
-        <header>
-            <section class="top-bar">
-                <div class="logo">
-                    Logo
-                </div>
-                <div class="search-box">
-                    Search
-                </div>
-            </section>
-            <section class="menu-area">
-                <nav class="main-menu">
-                    Main Menu
-                </nav>
-            </section>
-        </header>
+
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
