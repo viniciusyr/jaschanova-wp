361bb212857e0b13e5ea2f8442fd4d3f20958bfc
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2026-02-18 01:08:56 +0800

    feature: career card

--- app/public/wp-content/themes/jaschanova/functions.php
+++ app/public/wp-content/themes/jaschanova/functions.php
@@ -48,3 +48,78 @@ function register_testimonials_cpt() {
 }
 add_action('init', 'register_testimonials_cpt');
 
+function register_careers_cpt() {
+    register_post_type('careers', [
+        'labels' => [
+            'name' => 'Careers',
+            'singular_name' => 'Career',
+            'add_new' => 'Add Career',
+            'add_new_item' => 'Add New Career',
+            'edit_item' => 'Edit Career',
+        ],
+        'public' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'supports' => ['title'],
+        'menu_icon' => 'dashicons-briefcase',
+    ]);
+}
+add_action('init', 'register_careers_cpt');
+
+function register_acf_fields_for_careers() {
+    if ( function_exists( 'acf_add_local_field_group' ) ) {
+        acf_add_local_field_group( array(
+            'key' => 'group_careers',
+            'title' => 'Career Information',
+            'fields' => array(
+                array(
+                    'key' => 'field_career_logo',
+                    'label' => 'Company Logo',
+                    'name' => 'company_logo',
+                    'type' => 'image',
+                    'return_format' => 'array',
+                    'preview_size' => 'medium',
+                ),
+                array(
+                    'key' => 'field_career_title',
+                    'label' => 'Job Title',
+                    'name' => 'job_title',
+                    'type' => 'text',
+                ),
+                array(
+                    'key' => 'field_career_location',
+                    'label' => 'Location',
+                    'name' => 'location',
+                    'type' => 'text',
+                    'placeholder' => 'e.g., New York, NY',
+                ),
+                array(
+                    'key' => 'field_career_date',
+                    'label' => 'Date',
+                    'name' => 'career_date',
+                    'type' => 'text',
+                    'placeholder' => 'e.g., Jan 2023 - Dec 2024',
+                ),
+                array(
+                    'key' => 'field_career_description',
+                    'label' => 'Job Description',
+                    'name' => 'job_description',
+                    'type' => 'textarea',
+                    'rows' => 4,
+                    'placeholder' => 'Describe your responsibilities and achievements in this role.',
+                ),
+            ),
+            'location' => array(
+                array(
+                    array(
+                        'param' => 'post_type',
+                        'operator' => '==',
+                        'value' => 'careers',
+                    ),
+                ),
+            ),
+        ) );
+    }
+}
+add_action('acf/init', 'register_acf_fields_for_careers');
+
