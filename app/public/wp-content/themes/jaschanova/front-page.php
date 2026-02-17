549f8576ac0c2565905cb217b54223c0544e8cea
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2026-02-18 01:37:32 +0800

    feature: career section

--- app/public/wp-content/themes/jaschanova/front-page.php
+++ app/public/wp-content/themes/jaschanova/front-page.php
@@ -4,7 +4,6 @@
 <?php get_template_part('templates/services'); ?>
 <?php get_template_part('templates/career'); ?>
 <?php get_template_part('templates/about'); ?>
-<?php get_template_part('templates/skills'); ?>
 <?php get_template_part('templates/testimonials'); ?>
 <?php get_template_part('templates/va-life'); ?>
 <?php get_footer(); ?>
